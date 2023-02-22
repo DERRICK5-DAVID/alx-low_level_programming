f387c7638f3602565347da4d46bea777b7f87911
DERRICK-DAVID <ddoyconsult@gmail.com>
2023-02-21 16:24:59 -0800

    best programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single digit nums
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar(i + '0');
+		putchar(44);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
