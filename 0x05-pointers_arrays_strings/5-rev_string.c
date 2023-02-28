0fb102b094bf8ea81777becb71435ff866b53805
DERRICK-DAVID <ddoyconsult@gmail.com>
2023-02-27 18:51:18 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <string.h>
+/**
+ * rev_string - check the code
+ * @s: pointer charater array
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = strlen(s) - 1;
+	char tmp;
+	while (i < j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++;
+		j--;
+	}
+}
