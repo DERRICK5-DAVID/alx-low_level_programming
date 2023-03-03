7229a90d159c8d99a2df9505865846275ed20677
DERRICK-DAVID <ddoyconsult@gmail.com>
2023-03-03 15:36:14 -0800

    arrays

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * reverse_array -  reverses string
+ * @a: pointer to a
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	for(int i = 0; i < n/2; i++) {
+		temp = a[i];
+		a[i] = a[n-i-1];
+		a[n-i-1] = temp;
+	}
+}
